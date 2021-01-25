f7f04fb325c1c12b3097c0a487dd8b3fbf6115c4
sriDivya-WhiteHatJr <70732258+sridivya-1@users.noreply.github.com>
2021-01-25 19:24:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var database;
+var gameState = 0
+var playerCount;
+var form,player,game
+
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+
+  
+}
+
+function draw(){
+}
+
